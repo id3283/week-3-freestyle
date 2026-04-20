c729b5da233d283b4ad25d8c4b16fa3eda0eb8b6
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-04-20 16:31:04 +0100

    Added exception examples.

--- src/main/java/ExceptionHandling.java
+++ src/main/java/ExceptionHandling.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionHandling {
+
+    public static void main(String[] args) {
+        System.out.println("Enter an integer, please.");
+        Scanner myScanner = new Scanner(System.in);
+
+        boolean goodValue = false;
+        int i = 0;
+
+        do {
+            try {
+                String userInput = myScanner.nextLine();
+                i = Integer.parseInt(userInput);
+                goodValue = true;
+                System.out.println("If there's a problem, this won't print.");
+            }
+            catch (NumberFormatException e) {
+                System.out.println(e);
+                System.err.println("You didn't enter an integer.  Dummy.");
+            }
+        } while (goodValue == false);
+
+        System.out.println("You entered: " + i);
+    }
 }
--- src/main/java/SimpleException.java
+++ src/main/java/SimpleException.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class SimpleException {
+
+    public static void main(String[] args) {
+
+        try {
+            int i = 10 / 0;
+            System.out.println("this won't print.");
+
+        }
+        catch (ArithmeticException e) {
+            System.out.println("There was an error.");
+        }
+
+        System.out.println("done");
+    }
 }
